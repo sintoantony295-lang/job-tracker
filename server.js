cef4f554ea7376d3051964af41e9a5d708633c4c
Sinto-Antony <sintoantony295@gmail.com>
2026-08-03 17:28:01 +0530

    Update server for production

--- server.js
+++ server.js
@@ -29,6 +29,7 @@ app.get('/', (req, res) => {
 
 const PORT = process.env.PORT || 5000
 app.listen(PORT, () => {
-    console.log('server running on port 5000')
-})
-console.log(process.env.JWT_SECRET)
+    console.log(`Server running on port ${PORT}`);
+});
+
+// console.log(process.env.JWT_SECRET)
